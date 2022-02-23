f5d8d729ebe579b03883cfd1bd3c5eb54d8af4d0
Nallasamy-Muthupavithra <90901613+Nallasamy-Muthupavithra@users.noreply.github.com>
2022-02-23 07:52:59 +0530

    Add files via upload

---
+++ src/main/java/TypewiseAlert/AlertTargetContext.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Robert Bosch GmbH. All rights reserved.
+ */
+package TypewiseAlert;
+
+import TypewiseAlert.TypewiseAlert.BreachType;
+
+
+public class AlertTargetContext {
+
+  AlertTarget alertTarget;
+
+  public AlertTargetContext(final AlertTarget alertTarget) {
+    this.alertTarget = alertTarget;
+  }
+
+  public void sendAlertBasedOnTarget(final BreachType breachType) {
+    this.alertTarget.sendAlert(breachType);
+  }
+
+  public interface AlertTarget {
+
+    public void sendAlert(final BreachType breachType);
+
+    MailAlert mailAlert = TypewiseAlert::sendToEmail;
+    ControllerAlert contrlAlert = TypewiseAlert::sendToController;
+
+  }
+
+  public interface MailAlert extends AlertTarget {
+
+    public void sendAlert(final BreachType breachType);
+  }
+
+  public interface ControllerAlert extends AlertTarget {
+
+    public void sendAlert(final BreachType breachType);
+  }
+}
